ecfa649da2870e28199c0c1f6d81f527a7da177b
ngthuongdoan <ngthuongdoan@gmail.com>
2026-01-25 14:11:48 +0700

    Refactor deployment workflow: remove backup restoration step and enhance Node.js app setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
